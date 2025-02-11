6b31aec239ce7afe9391aacbe84061e7cfee8f50
Xiaoyang Wang <w18905415320@outlook.com>
2025-02-11 03:14:32 -0500

    first commit

---
+++ framerusercontent.com/sites/01ses04XyzgPQVbfVxXVUV/chunk-JK34HCG2.mjs
@@ -0,0 +1,2 @@
+function t(a,e){return{description:"hihi",favicon:"https://framerusercontent.com/assets/pIgeYaLBH4t20fjjf4DFrqBtc.png",robots:"max-image-preview:large",socialImage:"https://framerusercontent.com/assets/VcuWDO2CktTTPVsTsk5AMofd2Y.png",title:"Xiaoyang-Design"}}export{t as a};
+//# sourceMappingURL=chunk-JK34HCG2.mjs.map
